b4c6518115718b83a56cd34e75a92c14e321aa6b
Vinit Soni <vinitsoni9869@gmail.com>
2017-10-19 00:17:23 -0400

    add user post to db

--- main/users.py
+++ main/users.py
@@ -16,6 +16,14 @@ user_model = users_api.model('user', {
 singleUserParser = reqparse.RequestParser()
 singleUserParser.add_argument('username', location='args')
 
+one_user_post_parser = reqparse.RequestParser()
+one_user_post_parser.add_argument('username', type=str)
+one_user_post_parser.add_argument('email', type=str)
+
+new_user_fields = users_api.model('User', {
+	'username': fields.String,
+	'email': fields.String
+	})
 
 @users_api.route('/<string:username>')
 class SingleUser(Resource):
@@ -27,6 +35,14 @@ class SingleUser(Resource):
 			return person, 222
 		except Exception as e:
 			return {'server_error': str(e)}, 522
+
+	@users_api.expect(new_user_fields)
+	def get(self):
+		try:
+			args = one_user_post_parser.parse_args()
+			person = User(args['username'], args['email'])
+		except Exception as e:
+			raise e
 
 
 
